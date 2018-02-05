b39e33557e91790f58b0a454ab83d5bc4115d982
Haylee Fay Ham <hayleeham@gmail.com>
2018-02-05 12:15:30 -0600

    removing welcome controller

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index', to: 'welcome#index'
+  get 'articles/index', to: 'articles#index'
 
   resources :articles do
     resources :comments
   end
 
-  root 'welcome#index'
+  root 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
