4b3e0cf32f81aba052b1d98cd5581c45aea2be08
Andy Mockler <asmockler@gmail.com>
2018-06-25 21:50:59 -0400

    Remove duplicate directory

--- blog/db/migrate/20180212000028_connect_users_to_articles.rb
+++
@@ -1,8 +0,0 @@
-class ConnectUsersToArticles < ActiveRecord::Migration[5.1]
-  def change
-    create_table :articles_users do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :article, index: true
-    end
-  end
-end
