39ef7dd531a54d9cffec98dd2ab544cce2ce5bf8
Haylee Fay Ham <hayleeham@gmail.com>
2018-06-25 19:56:55 -0500

    broken hyperlinked tags

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments
+    resources :tags
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
